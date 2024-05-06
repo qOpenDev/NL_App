082cffb4d0d858eca3a3fbccee52f19ffb74733c
Kei Ito <51285238+Itookay@users.noreply.github.com>
2024-05-06 13:55:17 +0900

    認識結果を表示するresult.dartを追加

---
+++ noodlelens/lib/result.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class Result extends StatelessWidget {
+  const Result({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
